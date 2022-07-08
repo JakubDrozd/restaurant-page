e08cb6f3623b953993c869cf06dd21daa348b766
Veanty <plmulticraftpl@gmail.com>
2022-07-08 12:33:57 +0200

    Feat config for clear and self-cleaning webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    homePage: "./src/homePage.js",
+    menuPage: "./src/menuPage.js",
+    contactPage: "./src/contactPage.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant Page",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
